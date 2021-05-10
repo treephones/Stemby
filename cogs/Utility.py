cee17a7ee7eb0e40a8d47bea0e3a536d24806975
Moez B <moezbajwa@hotmail.com>
2021-05-10 19:56:27 -0400

    fixed utility file

--- cogs/Utility.py
+++ cogs/Utility.py
@@ -10,3 +10,6 @@ class Utility(commands.Cog):
     @commands.cooldown(1, 5, commands.BucketType.channel)
     async def ping(self, ctx):
         await ctx.send(embed=quick_embed(f"CroSenpai2's latency is **{round(self.bot.latency * 1000)}ms**!"))
+
+def setup(bot):
+    bot.add_cog(Utility(bot))
