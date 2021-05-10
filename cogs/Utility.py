6f7a2a6898c6a5bca39fe763e1c19cdb2369b102
Moez B <moezbajwa@hotmail.com>
2021-05-10 19:55:41 -0400

    added ping command

---
+++ cogs/Utility.py
@@ -0,0 +1,12 @@
+from discord.ext import commands
+from utils.embedutils import quick_embed
+
+class Utility(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.cooldown(1, 5, commands.BucketType.channel)
+    async def ping(self, ctx):
+        await ctx.send(embed=quick_embed(f"CroSenpai2's latency is **{round(self.bot.latency * 1000)}ms**!"))
