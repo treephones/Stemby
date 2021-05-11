ea61dfd178a288250135902da0ad3165a1e9f55e
Moez B <moezbajwa@hotmail.com>
2021-05-11 19:25:12 -0400

    vector module added

---
+++ modules/math_vect.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def dot_product(a,b):
+    a = np.array(a)
+    b = np.array(b)
+    return a.dot(b)
+
+def multiply(a, b):
+    a = np.array(a)
+    b = np.array(b)
+    return a*b
+
+def addition(a, b):
+    a = np.array(a)
+    b = np.array(b)
+    return a+b
+
+def subtraction(a, b):
+    a = np.array(a)
+    b = np.array(b)
+    return a-b
+
+def is_orthogonal(a, b): #format vector parameters as a list ([1,2], [2,-1])
+    a = np.array(a)
+    b = np.array(b)
+    return a.dot(b) == 0
+
+def ortho_proj(a,b): #orthogonal projection of b onto a
+    a = np.array(a)
+    b = np.array(b)
+
+    numerator = a.dot(b)
+    denominator = a.dot(a)
+
+    proj = (numerator/denominator)*a
+    return proj
