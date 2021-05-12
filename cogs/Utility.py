311b12169950d198178c5d4c7a10c19f310b6ec7
Moez B <moezbajwa@hotmail.com>
2021-05-11 21:25:40 -0400

    added alias command

--- cogs/Utility.py
+++ cogs/Utility.py
@@ -11,5 +11,16 @@ class Utility(commands.Cog):
     async def ping(self, ctx):
         await ctx.send(embed=quick_embed(f"CroSenpai2's latency is **{round(self.bot.latency * 1000)}ms**!"))
 
+    @commands.command(aliases=["alias"])
+    @commands.cooldown(1, 5, commands.BucketType.channel)
+    async def aliases(self, ctx, command):
+        try:
+            aliases = commands.Bot.get_command(self.bot, command).aliases
+            bs = "\n"
+            await ctx.send(embed=quick_embed(f"The aliases for the `{command}` command are:\n\n {bs.join(aliases)}"))
+        except Exception:
+            await ctx.send(embed=quick_embed(f"`{command}` is not a command!"))
+
+
 def setup(bot):
     bot.add_cog(Utility(bot))
