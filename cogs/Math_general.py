b730c4a4901898aec9fa49dbbee927b52d5d2f59
Moez B <moezbajwa@hotmail.com>
2021-05-13 18:54:16 -0400

    added unit conversions

--- cogs/Math_general.py
+++ cogs/Math_general.py
@@ -9,8 +9,14 @@ class Math_general(commands.Cog):
 
     @commands.command(aliases=["conv"])
     @commands.cooldown(1, 3, commands.BucketType.user)
-    async def convert(self, ctx, *, value, to_unit):
-        await ctx.send(embed=quick_embed(await math_general.convert(value, to_unit)))
+    async def convert(self, ctx, *, args):
+        args = args.split()
+        if len(args) == 2:
+            await ctx.send(embed=quick_embed(await math_general.convert(args[0], args[1])))
+        elif len(args) == 3:
+            await ctx.send(embed=quick_embed(await math_general.convert(f"{args[0]}{args[1]}", args[2])))
+        else:
+            await ctx.send(embed=quick_embed("Something went wrong! The command format is \n\n,convert `[value]` `[from]` `[to]`"))
 
 def setup(bot):
     bot.add_cog(Math_general(bot))
--- modules/math_general.py
+++ modules/math_general.py
@@ -2,11 +2,24 @@ from quantulum3 import parser
 from utils.mathutils import get_units
 import quantities as q
 
-def convert(fro, to):
-    fro = get_units(parser.parse(fro)[0].surface)
-    quant = q.Quantity(float(fro[0]), fro[1])
-    quant.units = to
-    return quant
-    #return strings
-
-print(convert("234s", "min"))
+async def convert(fro, to):
+    try:
+        fro = get_units(parser.parse(fro)[0].surface)
+        fro[1] = fro[1].strip()
+        #lazy check for temps lol
+        if to.strip().lower() in ["f", "c", "k"]:
+            to = f"deg{to}"
+        if fro[1].lower() in ["f", "c", "k"]:
+            fro[1] = f"deg{fro[1]}"
+        quant = q.Quantity(float(fro[0]), fro[1])
+        quant.units = to
+        ret = f"{' '.join(fro)} -> {quant}"
+    except IndexError:
+        ret = "Something went wrong! Check spelling and only use the actual word or official short form when writing units."
+    except ValueError:
+        ret = f"Something went wrong! Cannot convert between the units `{fro[1]}` and `{to}`!"
+    except LookupError:
+        ret = f"One of `{fro[1]}` or `{to}` are spelled incorrectly or do not exist! \n **NOTE**: Do not make short forms plural (ex, use \"sec\" rather than \"secs\" ;) )!"
+    except Exception:
+        ret = "Something went wrong!"
+    return ret
