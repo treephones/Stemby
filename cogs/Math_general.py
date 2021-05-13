edaac0636728dcfb1a1fb85f1d06c70fe37b4b3c
Moez B <moezbajwa@hotmail.com>
2021-05-13 19:49:24 -0400

    added basic expression evaluation

--- cogs/Math_general.py
+++ cogs/Math_general.py
@@ -18,5 +18,10 @@ class Math_general(commands.Cog):
         else:
             await ctx.send(embed=quick_embed("Something went wrong! The command format is \n\n,convert `[value]` `[from]` `[to]`"))
 
+    @commands.command(aliases=["eval"])
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def evaluate(self, ctx, *, expr):
+        await ctx.send(embed=quick_embed(await math_general.evaluate(expr)))
+
 def setup(bot):
     bot.add_cog(Math_general(bot))
--- modules/math_general.py
+++ modules/math_general.py
@@ -1,6 +1,8 @@
+import math
 from quantulum3 import parser
-from utils.mathutils import get_units
 import quantities as q
+from simpleeval import simple_eval, NumberTooHigh, InvalidExpression, FunctionNotDefined
+from utils.mathutils import get_units
 
 async def convert(fro, to):
     try:
@@ -20,6 +22,34 @@ async def convert(fro, to):
         ret = f"Something went wrong! Cannot convert between the units `{fro[1]}` and `{to}`!"
     except LookupError:
         ret = f"One of `{fro[1]}` or `{to}` are spelled incorrectly or do not exist! \n **NOTE**: Do not make short forms plural (ex, use \"sec\" rather than \"secs\" ;) )!"
+    except Exception:
+        ret = "Something went wrong!"
+    return ret
+
+functions = {"log": lambda x: math.log(x),
+                 "ln": lambda x: math.log(x, math.e),
+                 "sin": lambda x: math.sin(x),
+                 "cos": lambda x: math.cos(x),
+                 "tan": lambda x: math.tan(x),
+                 "arcsin": lambda x: math.asin(x),
+                 "arccos": lambda x: math.acos(x),
+                 "arctan": lambda x: math.atan(x),
+                 "sqrt": lambda x: math.sqrt(x)
+                 }
+
+async def evaluate(expression):
+    ret = "Evaluated expression:"
+    clean_expression = expression.replace("^", "**").replace("pi", str(math.pi)).replace("e", str(math.e))
+    try:
+        ret += f"```python\n" \
+              f"{expression}\n\n" \
+              f"=\n\n" \
+              f"{simple_eval(clean_expression, functions=functions)}" \
+              f"```"
+    except NumberTooHigh:
+        ret = "Something went wrong! That number is too large to evaluate!"
+    except InvalidExpression:
+        ret = "Something went wrong! That is an invalid expression!"
     except Exception:
         ret = "Something went wrong!"
     return ret
