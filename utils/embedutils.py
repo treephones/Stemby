9eed2750afc3ef2cfbe57c526580acfe51715a14
Moez B <moezbajwa@hotmail.com>
2021-05-19 18:00:18 -0400

    added date to embeds

--- utils/embedutils.py
+++ utils/embedutils.py
@@ -1,7 +1,10 @@
 from discord import Embed, Color
+from datetime import datetime
 
 def quick_embed(description, success=True):
-    return Embed(description=description, color=Embed.Empty if success else Color.dark_red())
+    embed = Embed(description=description, color=Embed.Empty if success else Color.dark_red())
+    embed.set_footer(text=datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+    return embed
 
 def mention(id):
     return f"<@{id}>"
