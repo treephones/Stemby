8ba027c4458c8375db26f8dee8c4482f7084c2dc
Moez B <moezbajwa@hotmail.com>
2021-05-22 18:31:00 -0400

    saves attachment and returns path

--- cogs/Memes.py
+++ cogs/Memes.py
@@ -1,9 +1,16 @@
+from discord import Attachment
 from discord.ext import commands
+from utils.cache import save_attachment
 
 class Memes(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    @commands.cooldown(2, 15, commands.BucketType.channel)
+    async def pepe(self, ctx):
+        path = save_attachment(ctx)
 
 def setup(bot):
     bot.add_cog(Memes(bot))
--- utils/cache.py
+++ utils/cache.py
@@ -2,3 +2,8 @@ from random import randint as rand
 
 def gen_cache_name():
     return "".join([str(rand(1,9)) for _ in range(8)])
+
+def save_attachment(ctx):
+    path = f'./caches/facereplace/{gen_cache_name()}.png'
+    await ctx.message.attachments[0].save(path)  # no attatchments error
+    return path
