7194a96b51ce12b9ff87035a243e368ea447114a
Moez B <moezbajwa@hotmail.com>
2021-05-27 15:51:32 -0400

    sucessfully iterated dictionary and gets most accurate hyperlink

--- modules/quiz.py
+++ modules/quiz.py
@@ -1,5 +1,19 @@
 import bs4 as bs
-from statics.quiz_topics import topics as t
+from random import choice
+from statics.quiz_topics import topics
 
 def get_link(topic):
-    pass
+    for key in topics.keys():
+        if key == topic:
+            rval1 = choice(list(topics[key].keys()))
+            subject = choice(topics[key][rval1]['topics'])
+            return topics[key][rval1]['link'].format(subject)
+        for subkey in topics[key].keys():
+            if subkey == topic:
+                subject = choice(topics[key][subkey]['topics'])
+                return topics[key][subkey]['link'].format(subject)
+            for subtopic in topics[key][subkey]['topics']:
+                if subtopic == topic:
+                    return topics[key][subkey]['link'].format(subtopic)
+    return None
+
--- test/test.py
+++ test/test.py
@@ -1,7 +1,10 @@
 from statics.quiz_topics import topics
+from modules.quiz import get_link
 
-for key in topics.keys():
-    for subkey in topics[key].keys():
-        link = topics[key][subkey]["link"]
-        for topic in topics[key][subkey]["topics"]:
-            print(link.format(topic))
+print(get_link("genetics"))
+
+# for key in topics.keys():
+#     for subkey in topics[key].keys():
+#         link = topics[key][subkey]["link"]
+#         for topic in topics[key][subkey]["topics"]:
+#             print(link.format(topic))
